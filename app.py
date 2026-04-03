dd37cacf80bfe68c51f67980c12b05a6b8f1bd20
Rinat <rinat.rinat.1302@gmail.com>
2026-04-03 22:26:55 +0300

    Основные импорты перенесены в config.py, первая api

--- app.py
+++ app.py
@@ -1,22 +1,5 @@
-from flask import Flask, render_template
-from werkzeug.utils import redirect
+from config import *
 
-from data import db_session
-from data.users import User
-
-from forms.login_form import LoginForm
-from forms.register_form import RegisterForm
-from flask_login import LoginManager, login_user, login_required, logout_user
-
-
-
-app = Flask(__name__)
-
-
-app.config["SECRET_KEY"] = "password123"
-
-login_manager = LoginManager()
-login_manager.init_app(app)
 
 @login_manager.user_loader
 def user_loader(user_id):
---
+++ config.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask_restful import Api
+from werkzeug.utils import redirect
+
+from data import db_session
+from data.users import User
+
+from forms.login_form import LoginForm
+from forms.register_form import RegisterForm
+from flask_login import LoginManager, login_user, login_required, logout_user
+
+from resources.users_resource import UsersResource, UsersListResource
+
+app = Flask(__name__)
+
+api = Api(app)
+api.add_resource(UsersResource, '/api/users/<int:user_id>')
+api.add_resource(UsersListResource, '/api/users/')
+
+app.config["SECRET_KEY"] = "password123"
+
+login_manager = LoginManager()
+login_manager.init_app(app)
